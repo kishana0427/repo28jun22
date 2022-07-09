95c75f3848682ecdd3b67ef02fc0c243ce4007fd
kishana0427 <95524882+kishana0427@users.noreply.github.com>
2022-07-09 12:14:26 +0530

    Update HellowWorld.java

--- HellowWorld.java
+++ HellowWorld.java
@@ -1,6 +1,6 @@
 class HellowWorld{
 public static void main(String[] args){
 System.out.println("Hello......Kishan ......!");
- System.out.println("Hello......Ki ......!");
+ System.out.println("Hello......KiSS ......!");
  }
 }
